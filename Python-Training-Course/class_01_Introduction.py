845bad0c81c61eb8107ae2d06f7f5fbbce329ff2
Rayhan Uddin <151814985+rayhan-uddin9@users.noreply.github.com>
2026-06-03 07:11:32 +0600

    Update class_01_Introduction.py

--- Python-Training-Course/class_01_Introduction.py
+++ Python-Training-Course/class_01_Introduction.py
@@ -1,5 +1,13 @@
 # Class 01 - Introduction to Python
 
+# Class 01 - Introduction to Python
+
+# Python was created by Guido van Rossum in 1991
+# Python runs line by line — no need to compile
+# print() shows text on the screen
+# Comments start with # — Python ignores them
+# Indentation means 4 spaces — very important in Python
+
 # Your first program
 print("Hello, World!")
 print("My name is Rayhan.")
